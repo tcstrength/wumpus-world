727da87b39e9116f901aac35c81dd0c9eb6dc265
zissalg <thai.cuong1404@gmail.com>
2019-07-22 12:55:11 +0700

    create new window

---
+++ wumpus-world.py
@@ -0,0 +1,9 @@
+print("Test vang ke ben")
+print("Test kho hon xiu")
+print("Khong co vang luon")
+print("1 hang` pits khong cho di qua")
+
+from ui.window import Window
+
+window = Window("Day la tro choi wumpus world hihi")
+window.run()
